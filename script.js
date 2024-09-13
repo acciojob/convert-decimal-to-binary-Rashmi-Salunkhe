272196388ec60fc48fb072fcf0b5fa1b762090c7
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-13 18:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function decimalToBinary(num) {
-  // your code here
+    if (num === 0) return '0'; // Handle the case for 0
+    return num.toString(2); // Convert the number to binary string
 }
-const num = parseInt(prompt("Enter a number."));
+
+const num = parseInt(prompt("Enter a number."), 10);
 alert(decimalToBinary(num));
